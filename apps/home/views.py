1b55ee20c885235d2f8a3c3f7dd5d7b0ed44344b
Ronny Archilinux <rapidtyping@hotmail.com>
2014-03-27 23:51:09 -0500

    cap6 - Formulario, enviar información con el método POST y dando seguridad con {% csrf_token %} en el template del formulario

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from apps.ventas.models import Producto
+from apps.home.forms import ContactForm
+
 
 def index_view(request):
     return render_to_response('home/index.html', 
@@ -18,5 +20,12 @@ def productos_view(request):
     mis_productos = Producto.objects.filter(status = True)
     ctx = {'productos': mis_productos}
     return render_to_response('home/productos.html',
+                              ctx,
+                              context_instance=RequestContext(request))
+
+def contacto_view(request):
+    formulario = ContactForm()
+    ctx = {'form':formulario}
+    return render_to_response('home/contacto.html',
                               ctx,
                               context_instance=RequestContext(request))
