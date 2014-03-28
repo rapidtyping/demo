0bca9e27f1f4b61e829bebdfd3de1e3dc4c6fb8c
Ronny Archilinux <rapidtyping@hotmail.com>
2014-03-27 23:25:19 -0500

    cap5 - Creamos la vista dinámica productos, en el template se usa los tags if y for, para verificar si hay productos activos y para iterar sobre los productos activos

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-
+from apps.ventas.models import Producto
 
 def index_view(request):
     return render_to_response('home/index.html', 
             context_instance=RequestContext(request))
 
 def about_view(request):
-    return render_to_response('home/about.html', 
+    mensaje = "Esto es un mesanje desde mi vista"
+    ctx = {'msg': mensaje}
+    return render_to_response('home/about.html',
+            ctx, 
             context_instance=RequestContext(request))
+
+def productos_view(request):
+    # Select * from ventas_productos where status = True
+    mis_productos = Producto.objects.filter(status = True)
+    ctx = {'productos': mis_productos}
+    return render_to_response('home/productos.html',
+                              ctx,
+                              context_instance=RequestContext(request))
